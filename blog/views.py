4724383ed5c70e6d11d813b8deb1645e8daa8870
Mukesh Kale <mukesh.kale09@gmail.com>
2017-10-17 03:59:02 -0400

    editor changes

--- blog/views.py
+++ blog/views.py
@@ -6,15 +6,26 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from django.template import loader
 from .models import Person
 
 def index(request):
-    latest_person_list = Person.objects.order_by('person_firstname')[:5]
-    output = ', '.join([q.person_firstname for q in latest_person_list])
-    return HttpResponse(output)
+    output = Person.objects.all()
+    template = loader.get_template('blog/index.html')
+    context = {
+        'output': output,
+    }
+    return HttpResponse(template.render(context, request))
+    #latest_person_list = Person.objects.order_by('person_firstname')[:5]
+    #output = ', '.join([q.person_firstname for q in latest_person_list])
+ #   html = ''
+  #  for i in output:
+   #     url = '/blog/' + str(i.id) + '/'
+    #    html += '<a href="' + url + '">' + i.person_firstname + '</a> <br>'
+    #return HttpResponse(html)
 
 #def index(request):
 #    return HttpResponse("Hello, world. You're at the polls index.")
 
 def detail(request, person_id):
-    return HttpResponse("You're looking at question %s." % person_id)
+    return HttpResponse("You're looking at person %s." % person_id)
